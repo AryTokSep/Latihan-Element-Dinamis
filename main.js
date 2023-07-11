577312cc29cfd082bec222cb1d225bf08abc3658
Thotok Ary Septiawan <135796207+AryTokSep@users.noreply.github.com>
2023-07-11 21:24:30 +0700

    Update main.js

--- main.js
+++ main.js
@@ -48,3 +48,22 @@ for (let i = 0; i < accordion.length; i++) {
     }
   });
 }
+
+//email
+var emailField = document.getElementById("email-field");
+var emailError = document.getElementById("email-error");
+
+function validateEmail(){
+
+    if(!emailField.value.match((/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/))) {
+        emailError.innerHTML = "Please enter a valid email";
+        emailError.style.color = "red";
+        
+        return false;
+    }
+
+    emailError.innerHTML = "email is true";
+    emailError.style.color = "green";
+    
+    return true;
+}
